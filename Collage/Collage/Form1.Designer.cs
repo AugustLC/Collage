6784163bd1b8ddf76f04fd8a11766b0420fca825
AugustLC <75476801+AugustLC@users.noreply.github.com>
2023-12-04 15:43:49 +0300

    Add files via upload

---
+++ Collage/Collage/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateBatton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generateBatton
+            // 
+            this.generateBatton.Location = new System.Drawing.Point(242, 378);
+            this.generateBatton.Name = "generateBatton";
+            this.generateBatton.Size = new System.Drawing.Size(150, 23);
+            this.generateBatton.TabIndex = 2;
+            this.generateBatton.Text = "Генерировать";
+            this.generateBatton.UseVisualStyleBackColor = true;
+            this.generateBatton.Click += new System.EventHandler(this.generateBatton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(170, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 324);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2000, 2000);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(22, 378);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 324);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0 root r\r\ni root images/1.jpg\r\nc c1 root\r\ni c1 images/2.jpg\r\ni c1 images/3.jpg";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ширина изображения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 413);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.generateBatton);
+            this.Name = "Form1";
+            this.Text = "Программа для создания коллажей";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateBatton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Collage/Collage/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //корень иерархического дерева построения коллажа
+        public static Node root;
+        //переменная для наименования промежуточных картинок коллажа
+        public static int i_count;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            update();
+            create();
+        }
+
+/*
+0 root c
+i root images/1.jpg
+r r1 root
+c c1 r1
+i c1 images/2.jpg
+i c1 images/1.jpg
+c c2 r1
+i c2 images/2.jpg
+i c2 images/1.jpg
+
+
+
+0 root r
+i root images/1.jpg
+c c1 root
+i c1 images/2.jpg
+i c1 images/3.jpg
+
+
+*/
+        
+
+        //обработка введённых команд
+        private void update()
+        {
+            string command;
+
+            command = textBox1.Text;
+
+            var result = command.Split(new[] { '\r', '\n' });
+
+            foreach (string r in result)
+            {
+                if (r != "")
+                {
+                    var c = r.Split(new[] { ' ' });
+                    if (c[0] == "i")
+                    {
+                        Zone zoneT = new Zone('i');
+                        zoneT.filename = c[2];
+
+                        root.setNode(c[1], "i", zoneT);
+                    }
+                    if (c[0] == "c")
+                    {
+                        Zone zoneT = new Zone('c');
+
+                        root.setNode(c[2], c[1], zoneT);
+                    }
+                    if (c[0] == "r")
+                    {
+                        Zone zoneT = new Zone('r');
+
+                        root.setNode(c[2], c[1], zoneT);
+                    }
+                    if (c[0] == "0")
+                    {
+                        Zone zone = new Zone(c[2][0]);
+                        root = new Node(c[1], zone);
+                    }
+                }
+            }
+
+        }
+
+        //рекурсивный поиск узла
+        private TreeNode searchNode(TreeNodeCollection node, string key)
+        {
+            foreach (TreeNode child in node)
+            {
+                searchNode(child.Nodes, key);
+                if (child.Text == key)
+                    return child;
+            }
+            return null;
+        }
+
+        //генерация коллажа
+        private Zone generate(Node node, char type)
+        {
+            List<Zone> images = new List<Zone>();
+            Zone tempIm;
+
+            foreach (Node n in node.nodes)
+            {
+                Zone temp = n.node;
+                
+                if (temp.type == 'i')
+                {
+                    tempIm = new Zone('0');
+                    i_count++;
+                    string path = "temp/" + i_count + ".jpg";
+                    DrawRow(new List<Zone>() { temp }).Save(path);
+                    tempIm.filename = path;
+                    images.Add(tempIm);
+                }
+                else
+                {
+                    tempIm = new Zone('0');
+                    tempIm = generate(n, temp.type);
+                    if (tempIm != null) images.Add(tempIm);
+                }
+                
+            }
+
+            if (type == 'c' && images.Count != 0)
+            {
+                i_count++;
+                string path = "temp/" + i_count + ".jpg";
+                DrawColumn(images).Save(path);
+                tempIm = new Zone('c');
+                tempIm.filename = path;
+                return tempIm;
+            }
+            if (type == 'r' && images.Count != 0)
+            {
+                i_count++;
+                string path = "temp/" + i_count + ".jpg";
+                DrawRow(images).Save(path);
+                tempIm = new Zone('r');
+                tempIm.filename = path;
+                return tempIm;
+            }
+
+            return null;
+            
+        }
+
+        //создание коллажа, отображение картинки и создание графического файла коллажа
+        private void create()
+        {
+            int width = (int)numericUpDown1.Value;
+
+            Bitmap image = new Bitmap(generate(root, root.node.type).filename);
+
+            float tempScale = (float)image.Width / width;
+            int newHeight = (int)(image.Height / tempScale);
+
+            pictureBox1.Width = width;
+            pictureBox1.Height = newHeight;
+
+            pictureBox1.Image = new Bitmap(5000, 5000);
+
+            Bitmap bmp;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            g.DrawImage(image, 0, 0, width, newHeight);
+
+            string path = "temp/ttt.jpg";
+            bmp.Save(path);
+
+            image = null;
+
+            GC.Collect();
+
+            Thread.Sleep(500);
+
+            DirectoryInfo dir = new DirectoryInfo("Temp");
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                if(f.Name != "ttt.jpg")
+                    f.Delete();
+            }
+        }
+        
+
+        //изменение ширины экрана
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            update();
+            create();
+        }
+
+        //кнопка генерации
+        private void generateBatton_Click(object sender, EventArgs e)
+        {
+            update();
+            create();
+        }
+
+        //рисование столбца
+        public Bitmap DrawColumn(List<Zone> zones)
+        {
+            Bitmap bmp;
+            bmp = new Bitmap(5000, 5000);
+            Graphics g = Graphics.FromImage(bmp);
+
+            int wStandart = 0;
+            int hStandart = 0;
+            int hTempRow = 0;
+
+            if (zones.Count > 0)
+            {
+                for (int i = 0; i < zones.Count; i++)
+                {
+                    Zone zone = zones[i];
+
+                    Bitmap image = new Bitmap(zone.filename);
+                    
+                    if (i == 0)
+                    {
+                        wStandart = image.Width;
+                        hStandart = image.Height;
+                        hTempRow = 0;
+                    }
+
+                    float tempScale = (float)image.Width / wStandart;
+                    int newHeight = (int)(image.Height / tempScale);
+
+                    g.DrawImage(image, 0, hTempRow, wStandart, newHeight);
+
+                    hTempRow += newHeight;
+                }
+
+                return bmp.Clone(new Rectangle(0, 0, wStandart, hTempRow), bmp.PixelFormat);
+            }
+
+            return null;
+        }
+
+        //рисование строки
+        public Bitmap DrawRow(List<Zone> zones)
+        {
+            Bitmap bmp;
+            bmp = new Bitmap(5000, 5000);
+            Graphics g = Graphics.FromImage(bmp);
+
+            int wStandart = 0;
+            int hStandart = 0;
+            int wTempRow = 0;
+            
+            if (zones.Count > 0)
+            {
+                for (int i = 0; i < zones.Count; i++ )
+                {
+                    Zone zone = zones[i];
+
+                    Bitmap image = new Bitmap(zone.filename);
+
+                    if (i == 0)
+                    {
+                        wStandart = image.Width;
+                        hStandart = image.Height;
+                        wTempRow = 0;
+                    }
+
+                    float tempScale = (float)image.Height / hStandart;
+                    int newWidth = (int)(image.Width / tempScale);
+
+                    g.DrawImage(image, wTempRow, 0, newWidth, hStandart);
+
+                    wTempRow += newWidth;
+
+                }
+
+                return bmp.Clone(new Rectangle(0, 0, wTempRow, hStandart), bmp.PixelFormat);
+            }
+            
+            return null;
+        }
+
+    }
+
+
+    //класс зоны для расчёта размеров промежуточных картинок коллажа
+    public class Zone
+    {
+        public Zone(char t) { type = t; }
+
+        public Zone(char t, int _x, int _y, int _w, int _h)
+        {
+            type = t;
+            x = _x;
+            y = _y;
+            w = _w;
+            h = _h;
+        }
+
+        public char type;
+        public string filename;
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+    }
+
+    //класс узла иерархического дерева для создания коллажа
+    public class Node
+    {
+        public Node(string _name, Zone _node) 
+        {
+            node = _node;
+            nodes = new List<Node>();
+            name = _name;
+            node = _node; 
+        }
+
+        public string name;
+
+        public Zone node;
+
+        public List<Node> nodes;
+
+        public void setNode(string _name, Zone _node)
+        {
+            Node n = new Node(_name, _node);
+            nodes.Add(n);
+        }
+
+        public void setNode(string parent, string _name, Zone _node)
+        {
+            foreach (Node n in nodes)
+            {
+                n.setNode(parent, _name, _node);
+            }
+            if (name == parent)
+            {
+                Node n1 = new Node(_name, _node);
+                nodes.Add(n1);
+            }
+        }
+
+        public Node getNode(string _key)
+        {
+            if (name == _key) return this;
+            return getNode(_key,  nodes);
+        }
+
+        public Node getNode(string _key, List<Node> nodes)
+        {
+            
+            foreach (Node n in nodes)
+            {
+                getNode(_key, n.nodes);
+                if (n.name == _key)
+                {
+                    return n;
+                }
+            }
+
+            return null;
+        }
+
+
+        public void output()
+        {
+            MessageBox.Show(node.type.ToString());
+            foreach (Node n in nodes)
+            {
+                n.output();
+            }
+        }
+
+    }
+}
